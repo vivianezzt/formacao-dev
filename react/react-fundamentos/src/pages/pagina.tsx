3d0b8ed75cffd37b782a454521d93f655a8c632f
Viviane <vivianeaguiarc@outlook.com>
2025-01-04 12:54:05 -0300

    chore: react-fundamentos pagina

--- react/react-fundamentos/src/pages/pagina.tsx
+++ react/react-fundamentos/src/pages/pagina.tsx
@@ -1,7 +1,19 @@
-export default function Pagina(){
-    return(
-        <div>
-            Estou na pasta pages
-        </div>
-    )
+import Cabecalho from "@/components/Cabecalho";
+import "../app/globals.css";
+import Conteudo from "@/components/Conteudo";
+import Rodape from "@/components/Rodape";
+export default function Pagina() {
+  return (
+    <div
+      className={`
+            flex flex-col 
+            h-screen
+            p-5 gap-5
+            bg-black text-white`}
+    >
+      <Cabecalho titulo="Minha Página" subtitulo="estou na pasta pages" />
+      <Conteudo />
+      <Rodape />
+    </div>
+  );
 }
